cbcfd789a3c7c0597a906df850f866571960dd3f
ygvbhy <ygvbhy@naver.com>
2024-03-10 20:35:17 +0900

    로컬 스토리지 데이터 조회 함수 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 
+function fetchTodos() {
+  const result = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    result.push(localStorage.key(i));
+  }
+
+  return result;
+}
+
 function App() {
   // const [count, setCount] = useState(0);
   const [inputText, setInputText] = useState("");
+  const todos = fetchTodos();
 
   const handleInput = (event) => {
     const value = event.target.value;
@@ -20,6 +30,13 @@ function App() {
       <div>
         <input type="text" value={inputText} onChange={handleInput} />
         <button onClick={handleClick}>add</button>
+      </div>
+      <div>
+        <ul>
+          <li>할일 추가</li>
+          <li>할일 조회</li>
+          <li>할일 삭제</li>
+        </ul>
       </div>
     </div>
   );
